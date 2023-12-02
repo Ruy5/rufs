8d8d9c1cfce24c04b85c1a6e6a15e831713050aa
RU <ru@feli.top>
2023-12-02 22:28:09 +0800

    file server init

---
+++ src/test/java/top/feli/wlt/WltApplicationTests.java
@@ -0,0 +1,13 @@
+package top.feli.wlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
